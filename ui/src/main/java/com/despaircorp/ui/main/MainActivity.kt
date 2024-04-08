c75118f87195d7abe0f03b0d8657a16dc57d455f
monokouma <haaxiz@gmail.com>
2024-04-08 23:34:20 +0200

    lets goooooo

--- ui/src/main/java/com/despaircorp/ui/main/MainActivity.kt
+++ ui/src/main/java/com/despaircorp/ui/main/MainActivity.kt
@@ -1,36 +1,50 @@
 package com.despaircorp.ui.main
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.despaircorp.domain.picture.model.EstatePictureType
 import com.despaircorp.ui.R
 import com.despaircorp.ui.databinding.ActivityMainBinding
 import com.despaircorp.ui.databinding.AddAgentPopUpBinding
+import com.despaircorp.ui.databinding.AddEstatePopUpBinding
 import com.despaircorp.ui.databinding.AddingChoicePopUpBinding
 import com.despaircorp.ui.databinding.HeaderNavigationDrawerBinding
 import com.despaircorp.ui.login.LoginActivity
+import com.despaircorp.ui.main.details_fragment.DetailFragment
+import com.despaircorp.ui.main.master_fragment.MasterFragment
+import com.despaircorp.ui.utils.uriToBitmap
 import com.despaircorp.ui.utils.viewBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+class MainActivity : AppCompatActivity(), MasterFragment.OnItemSelectedListener {
     private val binding by viewBinding { ActivityMainBinding.inflate(it) }
     private val viewModel: MainViewModel by viewModels()
+    private var twoPane: Boolean = false
+    private var currentSelectedItemId: Int = 1
     
-    private lateinit var navController: NavController
+    private val pickImageResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.data?.let { uri ->
+                    showImageTypeDialog(uri)
+                }
+            }
+        }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,17 +52,40 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         setContentView(binding.root)
         setSupportActionBar(binding.activityMainToolbarRoot)
         
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(binding.activityMainFragmentContainerView.id)
-                    as NavHostFragment
-        navController = navHostFragment.findNavController()
-        
-        navController.addOnDestinationChangedListener(this)
+        twoPane = binding.activityMainFragmentLayoutDetails != null
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_main_FragmentLayout_master, MasterFragment())
+                .commit()
+            
+            if (twoPane) {
+                val detailsFragment = DetailFragment.newInstance(currentSelectedItemId)
+                
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.activity_main_FragmentLayout_details, detailsFragment)
+                    .commit()
+            }
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_main_FragmentLayout_master, MasterFragment())
+                .commit()
+            if (twoPane) {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportFragmentManager.popBackStack()
+                }
+                val detailsFragment =
+                    DetailFragment.newInstance(savedInstanceState.getInt("selectedItemId", 1))
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.activity_main_FragmentLayout_details, detailsFragment)
+                    .commit()
+            }
+        }
         
         
         val headerBinding = HeaderNavigationDrawerBinding.bind(
             binding.activityMainNavigationViewProfile.getHeaderView(0)
         )
+        
         
         binding.activityMainToolbarRoot.setNavigationOnClickListener {
             binding.activityMainDrawerLayout.open()
@@ -97,6 +134,26 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
             headerBinding.navigationDrawerTextViewUserName.text = it.currentLoggedInAgent.name
         }
         
+    }
+    
+    override fun onResume() {
+        super.onResume()
+        
+    }
+    
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        
+        outState.putInt("selectedItemId", currentSelectedItemId)
+        
+    }
+    
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
     }
     
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -167,8 +224,36 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
     }
     
     private fun onAddEstateClicked() {
-    
+        val popUpBinding by viewBinding { AddEstatePopUpBinding.inflate(it) }
+        
+        val dialog =
+            MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_MaterialAlertDialog_Rounded)
+                .setView(popUpBinding.root)
+                .create()
+        
+        dialog.show()
+        
+        popUpBinding.addEstatePopUpButtonAddPicture.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            pickImageResultLauncher.launch(intent)
+        }
     }
+    
+    private fun showImageTypeDialog(imageUri: Uri) {
+        val imageTypes = EstatePictureType.entries.toTypedArray()
+        val items = imageTypes.map { it.name.capitalize(Locale.getDefault()) }.toTypedArray()
+        
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Select Image Type")
+            .setItems(items) { _, which ->
+                val selectedType = imageTypes[which]
+                uriToBitmap(this, imageUri)
+                
+            }
+            .show()
+    }
+    
     
     companion object {
         
@@ -178,12 +263,20 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         )
     }
     
-    override fun onDestinationChanged(
-        controller: NavController,
-        destination: NavDestination,
-        arguments: Bundle?
-    ) {
-    
-    
+    override fun onItemSelected(itemId: Int) {
+        currentSelectedItemId = itemId
+        if (twoPane) {
+            val detailsFragment = DetailFragment.newInstance(itemId)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_main_FragmentLayout_details, detailsFragment)
+                .commit()
+        } else {
+            val detailsFragment = DetailFragment.newInstance(itemId)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_main_FragmentLayout_master, detailsFragment)
+                .addToBackStack("detail")
+                .commit()
+        }
     }
+    
 }
